a8a81e8ab3d748b699a14fc82322171c20b30578
sasitha12 <sasithahasaranga23@gmail.com>
2025-04-30 16:01:56 +0530

    Setting up the dashboard

--- app.py
+++ app.py
@@ -1,2 +1,25 @@
 import streamlit as st
-st.write("Hi")
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import calendar
+
+st.set_page_config(
+    page_title="Sri Lanka Events and Fatalities Dashboard",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+@st.cache_data
+def load_data():
+    df = pd.read_csv("cleaned_dataset.csv")
+    df['Date'] = pd.to_datetime(df['Year'].astype(str) + '-' + df['Month'] + '-01')
+    df['Month_Year'] = df['Month'] + ' ' + df['Year'].astype(str)
+    return df
+
+df = load_data()
+
+#sidebar
+st.sidebar.title("Navigation")
+page = st.sidebar.radio("Go to", ["About", "Dashboard"])
